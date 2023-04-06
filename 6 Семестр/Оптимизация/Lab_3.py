744e59158412dc327a56617c7a38166746ab255d
ALEX <alexsmi0807@mail.ru>
2023-04-06 17:25:51 +0300

    Change in lab_3 optimization

--- 6 Семестр/Оптимизация/Lab_3.py
+++ 6 Семестр/Оптимизация/Lab_3.py
@@ -37,7 +37,6 @@ def method_of_gradient_descent_with_a_constant_step(x1, x2, e, M):
         grad = gradient(x1, x2)
         module_grad = module_of_gradient(grad)
         
-        counter_1 += 1
         if ((module_grad < e) | (k >= M)):
             return [(round(x1, round_num), round(x2, round_num), round(f(x1, x2), round_num)), k]
 
@@ -51,11 +50,10 @@ def method_of_gradient_descent_with_a_constant_step(x1, x2, e, M):
             gamma /= 2
             x1_next = x1 - gamma * grad[0]
             x2_next = x2 - gamma * grad[1]
-            counter_1 += 2
+            counter_1 += 1
         
         #print(grad, 'x1 =', x1, 'x2 =', x2, 'x1_next =', x1_next, 'x2_next =', x2_next, 'gamma =', gamma)
         
-        counter_1 += 2
         x1_list.append(x1); x2_list.append(x2)
         if ((sqrt(abs(x1_next - x1)**2 + abs(x2_next - x2)**2) <= e*e)
             & (abs(f(x1_next, x2_next) - f(x1, x2)) <= e*e)):
@@ -70,56 +68,43 @@ def method_of_gradient_descent_with_a_constant_step(x1, x2, e, M):
 
 # ------------------------------------------------------------------------------------
 
+def f_lambda(x1, x2, l, dx1f, dx2f):
+    return f(x1 - l*dx1f, x2 - l*dx2f)
 
-def f_1_gamma(x1, x2, gamma):
-    global counter_2
-    return x1 - gamma * f_x1(x1, x2)
-
-def f_2_gamma(x1, x2, gamma):
-    global counter_2
-    return x2 - gamma * f_x2(x1, x2)
-
-def dichotomy_mehod(a, b, epsilon, x1, x2, num):
-    x = (a + b) / 2
+def dichotomy_mehod(a, b, epsilon, x1, x2, dx1f, dx2f):
+    l = (a + b) / 2
     global counter_2
     counter_2 += 2
-    if num == 1:
-        if (f(f_1_gamma(x1, x2, x - epsilon), f_2_gamma(x1, x2, x)) < f(f_1_gamma(x1, x2, x + epsilon), f_2_gamma(x1, x2, x))):
-            b = x
-        else:
-            a = x
+
+    if (f_lambda(x1, x2, l - epsilon, dx1f, dx2f) < f_lambda(x1, x2, l + epsilon, dx1f, dx2f)):
+        b = l
     else:
-        if (f(f_1_gamma(x1, x2, x), f_2_gamma(x1, x2, x - epsilon)) < f(f_1_gamma(x1, x2, x) , f_2_gamma(x1, x2, x + epsilon))):
-            b = x
-        else:
-            a = x
+        a = l
 
-    counter_2 += 1
     if(abs(b - a) >= 2 * epsilon):
-        return dichotomy_mehod(a, b, epsilon, x1, x2, num)
-    return x
+        return dichotomy_mehod(a, b, epsilon, x1, x2, dx1f, dx2f)
+    return l
 
-def method_of_the_steepest_gradient_descent(x1, x2, e, M):
+def method_of_the_steepest_gradient_descent(x1, x2, e1, e2, M):
     global counter_2
     k = 0
     while True:
         counter_2 += 2
         grad = gradient(x1, x2)
         module_grad = module_of_gradient(grad)
-        if ((module_grad < e) | (k >= M)):
+        if ((module_grad < e1) | (k >= M)):
             return [(round(x1, round_num), round(x2, round_num), round(f(x1, x2), round_num)), k]
 
-        gamma_x1 = dichotomy_mehod(0, 0.1, e, x1, x2, 1)
-        gamma_x2 = dichotomy_mehod(0, 0.1, e, x1, x2, 2)
+        gamma = dichotomy_mehod(0, 0.1, e2, x1, x2, grad[0], grad[1])
 
-        x1_next = x1 - gamma_x1 * grad[0]
-        x2_next = x2 - gamma_x2 * grad[1]
+        x1_next = x1 - gamma * grad[0]
+        x2_next = x2 - gamma * grad[1]
 
-        #print(grad, 'x1 =', x1, 'x2 =', x2, 'gamma_1 =', gamma_x1, 'gamma_2 =',gamma_x2)
+        #print(grad, 'x1 =', x1, 'x2 =', x2, 'gamma =', gamma)
 
         #x1_list.append(x1); x2_list.append(x2)
-        if ((sqrt(abs(x1_next - x1)**2 + abs(x2_next - x2)**2) <= e*e)
-            & (abs(f(x1_next, x2_next) - f(x1, x2)) <= e*e)):
+        if ((sqrt(abs(x1_next - x1)**2 + abs(x2_next - x2)**2) <= e2)
+            & (abs(f(x1_next, x2_next) - f(x1, x2)) <= e2)):
             return [(round(x1_next, round_num), 
                      round(x2_next, round_num), 
                      round(f(x1_next, x2_next), round_num)), 
@@ -135,9 +120,12 @@ def show(x1_list, x2_list):
         return
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    
+    #x1_array = np.arange(min(x1_list) - 1, max(x1_list) + 1, 0.01)
+    #x2_array = np.arange(min(x2_list) - 1, max(x2_list) + 1, 0.01)
 
-    x1_array = np.arange(-1, 1.2, 0.01)
-    x2_array = np.arange(0, 1.2, 0.01)
+    x1_array = np.arange(-1.2, 1.2, 0.01)
+    x2_array = np.arange(0.2, 1.2, 0.01)
 
     x1_array, x2_array = np.meshgrid(x1_array, x2_array)
     R = f(x1_array, x2_array)
@@ -166,6 +154,11 @@ def show(x1_list, x2_list):
     x2_list2.append(x2_list[0])
     f_list.append(f(x1_list[0], x2_list[0]))
 
+    #ax.scatter(x1_list[1], x2_list[1], f(x1_list[1], x2_list[1]), c='red')
+    #x1_list2.append(x1_list[1])
+    #x2_list2.append(x2_list[1])
+    #f_list.append(f(x1_list[1], x2_list[1]))
+
     for n in range(r, N - 1*r, r):
 
         ax.scatter(x1_list[n], x2_list[n], f(x1_list[n], x2_list[n]), c='red')
@@ -184,20 +177,20 @@ def show(x1_list, x2_list):
     #plt.ioff()   # выключение интерактивного режима отображения графиков
 
     plt.show()
-    input()
-    
+
 
 x1 = 0.5
 x2 = 0.5
-e = 0.0001
+e1 = 0.001
+e2 = 0.001 * 0.001
 round_num = 4
-M = 20000
+M = 10000
 
-result = method_of_gradient_descent_with_a_constant_step(x1, x2, e, M)
+result = method_of_gradient_descent_with_a_constant_step(x1, x2, e1, M)
 print(f"The method of gradient descent with a constant step: {result[0]}; count of iteractions = {result[1]}")
 print('Count of compute function =', counter_1)
 
-result = method_of_the_steepest_gradient_descent(x1, x2, e, M)
+result = method_of_the_steepest_gradient_descent(x1, x2, e1, e2, M)
 print(f"The method of the steepest gradient descent: {result[0]}; count of iteractions = {result[1]}")
 print('Count of compute function =', counter_2)
 
